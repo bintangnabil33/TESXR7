ce47af34684c37ae39dc0cc45b04f5da917d5e92
bintang <muhammad_widyawan_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 13:55:45 +0700

    Add files via upload

---
+++ tes.java
@@ -0,0 +1,6 @@
+public class tes {
+    public static void main(String[] args){
+        System.out.println("Nama saya bintang dari kelas X RPL 7 yang berasal dari kota tulungagung");
+        String a = "1"; 
+    }
+}
