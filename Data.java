58456ba84f1c9398f225f22012bf31ae58d2b299
bintang <muhammad_widyawan_33rpl@student.smktelkom-mlg.sch.id>
2024-08-05 11:40:15 +0700

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,14 @@
+public class Data {
+    public static void main(String[] args) {
+        //tipe_data nama_var = value/isi var;
+        String nama = "bintang";
+        int usia = 14;
+        String alamat = "Jl. danau ranau raya";
+        double kelas = 10.7;
+
+        System.out.println("nama saya " + nama);
+        System.out.println("Usia :" + usia + " tahun ");
+        System.out.println("Alamat : " + alamat);
+        System.out.println("Kelas : " + kelas);
+    }
+}
